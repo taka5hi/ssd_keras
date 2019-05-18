917cb2e75082e57551cad01db88a669d53fd000b
taka5hi <n.takashi930@gmail.com>
2019-05-18 19:55:57 +0900

    fix error due to module versions

--- ssd.py
+++ ssd.py
@@ -9,7 +9,7 @@ from keras.layers import Flatten
 from keras.layers import GlobalAveragePooling2D
 from keras.layers import Input
 from keras.layers import MaxPooling2D
-from keras.layers import merge
+from keras.layers import concatenate
 from keras.layers import Reshape
 from keras.layers import ZeroPadding2D
 from keras.models import Model
@@ -249,28 +249,27 @@ def SSD300(input_shape, num_classes=21):
                                     name='pool6_reshaped')(net['pool6'])
     net['pool6_mbox_priorbox'] = priorbox(net['pool6_reshaped'])
     # Gather all predictions
-    net['mbox_loc'] = merge([net['conv4_3_norm_mbox_loc_flat'],
-                             net['fc7_mbox_loc_flat'],
-                             net['conv6_2_mbox_loc_flat'],
-                             net['conv7_2_mbox_loc_flat'],
-                             net['conv8_2_mbox_loc_flat'],
-                             net['pool6_mbox_loc_flat']],
-                            mode='concat', concat_axis=1, name='mbox_loc')
-    net['mbox_conf'] = merge([net['conv4_3_norm_mbox_conf_flat'],
-                              net['fc7_mbox_conf_flat'],
-                              net['conv6_2_mbox_conf_flat'],
-                              net['conv7_2_mbox_conf_flat'],
-                              net['conv8_2_mbox_conf_flat'],
-                              net['pool6_mbox_conf_flat']],
-                             mode='concat', concat_axis=1, name='mbox_conf')
-    net['mbox_priorbox'] = merge([net['conv4_3_norm_mbox_priorbox'],
-                                  net['fc7_mbox_priorbox'],
-                                  net['conv6_2_mbox_priorbox'],
-                                  net['conv7_2_mbox_priorbox'],
-                                  net['conv8_2_mbox_priorbox'],
-                                  net['pool6_mbox_priorbox']],
-                                 mode='concat', concat_axis=1,
-                                 name='mbox_priorbox')
+    net['mbox_loc'] = concatenate([net['conv4_3_norm_mbox_loc_flat'],
+                                  net['fc7_mbox_loc_flat'],
+                                  net['conv6_2_mbox_loc_flat'],
+                                  net['conv7_2_mbox_loc_flat'],
+                                  net['conv8_2_mbox_loc_flat'],
+                                  net['pool6_mbox_loc_flat']],
+                                  axis=1, name='mbox_loc')
+    net['mbox_conf'] = concatenate([net['conv4_3_norm_mbox_conf_flat'],
+                                   net['fc7_mbox_conf_flat'],
+                                   net['conv6_2_mbox_conf_flat'],
+                                   net['conv7_2_mbox_conf_flat'],
+                                   net['conv8_2_mbox_conf_flat'],
+                                   net['pool6_mbox_conf_flat']],
+                                   axis=1, name='mbox_conf')
+    net['mbox_priorbox'] = concatenate([net['conv4_3_norm_mbox_priorbox'],
+                                       net['fc7_mbox_priorbox'],
+                                       net['conv6_2_mbox_priorbox'],
+                                       net['conv7_2_mbox_priorbox'],
+                                       net['conv8_2_mbox_priorbox'],
+                                       net['pool6_mbox_priorbox']],
+                                       axis=1, name='mbox_priorbox')
     if hasattr(net['mbox_loc'], '_keras_shape'):
         num_boxes = net['mbox_loc']._keras_shape[-1] // 4
     elif hasattr(net['mbox_loc'], 'int_shape'):
@@ -281,10 +280,9 @@ def SSD300(input_shape, num_classes=21):
                                name='mbox_conf_logits')(net['mbox_conf'])
     net['mbox_conf'] = Activation('softmax',
                                   name='mbox_conf_final')(net['mbox_conf'])
-    net['predictions'] = merge([net['mbox_loc'],
-                               net['mbox_conf'],
-                               net['mbox_priorbox']],
-                               mode='concat', concat_axis=2,
-                               name='predictions')
+    net['predictions'] = concatenate([net['mbox_loc'],
+                                     net['mbox_conf'],
+                                     net['mbox_priorbox']],
+                                     axis=2, name='predictions')
     model = Model(net['input'], net['predictions'])
     return model
